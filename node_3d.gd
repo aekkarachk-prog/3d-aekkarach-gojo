19e78ee40052dba3fc8d8df89e811a32e77f5183
Spren11 <196334223+Spren11@users.noreply.github.com>
2025-09-22 21:39:57 +0700

    Done
    F

--- node_3d.gd
+++ node_3d.gd
@@ -6,6 +6,7 @@ func _ready() -> void:
 	$Walking/AnimationPlayer.play("walk")
 	$Walking2/AnimationPlayer.play("run")
 	$Walking3/AnimationPlayer.play("die")
+	$Punching/AnimationPlayer.play("punch")
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
